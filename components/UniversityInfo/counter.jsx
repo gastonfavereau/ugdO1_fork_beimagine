7f55ad33cd9d9e06b65b109d62e96e1b2d36d09f
hemantKumar14 <hemant@beimagine.tech>
2023-12-14 23:46:19 +0530

    review Changes

--- components/UniversityInfo/counter.jsx
+++ components/UniversityInfo/counter.jsx
@@ -5,7 +5,7 @@ import "./aboutUs.css";
 export default function Counter({ number, title }) {
   return (
     <div className="number">
-      <CountUp duration={30} className="counter" end={number} />
+      <CountUp duration={5} className="counter" end={number} />
       <span>{title}</span>
     </div>
   );

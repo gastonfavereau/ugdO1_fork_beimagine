4e04fb3b8ebdef2e76114ae6451afa4e78a871f1
beimagine <155222377+beimagine@users.noreply.github.com>
2024-03-12 23:06:41 +0530

    Update home.jsx

--- components/home/home.jsx
+++ components/home/home.jsx
@@ -21,7 +21,7 @@ const HomePage = ({params}) => {
   return (
     <div className='home'>
       <video id="background-video" autoPlay loop muted poster="/UGDVposter.webp">
-        <source src="UGDVideo2.mp4" type="video/mp4" />
+        <source src="ugdMediumVideo.mp4" type="video/mp4" />
       </video><video id="backgroundMobile" autoPlay loop muted poster="/UGDVposter.webp">
         <source src="UGDVideoMobile2.mp4" type="video/mp4" />
       </video>
